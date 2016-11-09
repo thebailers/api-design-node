237ef2dcc45280a4a0cde60815d48412a8b9c320
Simon <simon@pelli.co.uk>
2016-11-09 16:56:39 +0000

    Solution to exercie 2

--- server/server.js
+++ server/server.js
@@ -22,6 +22,50 @@ var lions = [];
 var id = 0;
 
 // TODO: make the REST routes to perform CRUD on lions
+app.get('/lions', function(req, res) {
+  res.json(lions)
+})
+
+app.get('/lions/:id', function(req, res) {
+  var lion = _.find(lions, {id: req.params.id})
+  res.json(lion || {})
+})
+
+app.post('/lions', function(req, res) {
+  var lion = req.body
+  id++
+  lion.id = id + ''
+
+  lions.push(lion)
+
+  res.json(lion)
+})
+
+app.put('/lions/:id', function(req, res) {
+  var update = req.body
+  if (update.id) {
+    delete update.id
+  }
+
+  var lion = _.findIndex(lions, {id: req.params.id}) // finds the 
+  if (!lions[lion]) {
+    res.send()
+  } else {
+    var updatedLion = _.assign(lions[lion], update) // merges the objects, and returns the object on the left
+    res.json(updatedLion)
+  }
+})
+
+app.delete('/lions/:id', function(req, res) {
+  var lion = _.findIndex(lions, {id: req.params.id})
+  if (!lions[lion]) {
+    res.send()
+  } else {
+    var deletedLion = lions[lion]
+    lions.splice(lion, 1)
+    res.json(deletedLion)
+  }
+})
 
 app.listen(3000);
 console.log('on port 3000');
